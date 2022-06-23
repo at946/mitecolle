bae0e2b06cb2239da5ff1eb81c8dc2ec9c68eb68
Asato TAKAHASHI <asato730@gmail.com>
2022-06-23 20:10:07 +0900

    nend

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       GET_RANKING_URL: string;
+      NEND_IS_ACTIVE: boolean;
       NEXT_PUBLIC_GA_ID?: string;
       NEXT_PUBLIC_VERCEL_URL?: string;
     }
