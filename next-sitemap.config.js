2aa7d00ffb1a45c0170916b45274cd992757d40b
Lint Action <lint-action@samuelmeuli.com>
2022-06-26 11:12:13 +0000

    Fix code style issues with Prettier

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next-sitemap').IConfig} */
-const siteUrl = process.env.NEXT_PUBLIC_VERCEL_URL ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` : 'http://localhost:3000'
+const siteUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : 'http://localhost:3000';
 
 module.exports = {
   siteUrl: siteUrl,
   generateRobotsTxt: true,
-}
+};
